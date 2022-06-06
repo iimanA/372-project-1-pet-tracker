a51893e356afa7a9fcdd609431882c48f7e4c080
Julia Ha <juliaha0902@gmail.com>
2022-06-06 08:10:59 -0500

    Finish PetCompany class

--- Project1/src/main/project1/company/PetCompany.java
+++ Project1/src/main/project1/company/PetCompany.java
@@ -3,6 +3,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import org.json.simple.JSONArray;
@@ -19,13 +20,30 @@ public class PetCompany {
     }
 
     public PetCompany(String fileName) {
+        shelterList = new HashMap<String, Shelter>();
+        addAnimal(fileName);
+    }
+
+    public void addAnimal (String fileName) {
         Path resourceDirectory = Path.of("src", "resources", fileName);
         String absolutePath = resourceDirectory.toFile().getAbsolutePath();
-        System.out.println (absolutePath);
+//        System.out.println (absolutePath);
         try {
             Object obj = new JSONParser().parse(new FileReader(absolutePath));
-            JSONObject jo = (JSONObject) obj;
-            System.out.println(jo);
+            JSONObject jsonObject = (JSONObject) obj;
+            for (Object key : jsonObject.keySet()) {
+                JSONArray animalList = (JSONArray) jsonObject.get(key);
+                for (int i = 0; i < animalList.size(); i++) {
+                    JSONObject animalJson = (JSONObject) animalList.get(i);
+                    Animal animal = new Animal(animalJson);
+                    String shelterId = animal.getShelterId();
+                    if (shelterList.get(shelterId) == null) {
+                        shelterList.put(shelterId, new Shelter(shelterId));
+                    }
+                    Shelter shelter = shelterList.get(shelterId);
+                    shelter.addAnimal (animal);
+                }
+            }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -33,6 +51,26 @@ public class PetCompany {
         } catch (ParseException e) {
             e.printStackTrace();
         }
+    }
+
+    public void enableReceivingAnimal (String shelterId) {
+        Shelter shelter = shelterList.get(shelterId);
+        shelter.setInTaking (true);
+    }
+
+    public void disableReceivingAnimal (String shelterId) {
+        Shelter shelter = shelterList.get(shelterId);
+        shelter.setInTaking (false);
+    }
+
+    public JSONObject exportAnimalListJSON (String shelterId) {
+        Shelter shelter = shelterList.get(shelterId);
+        return shelter.exportAnimalListJSON ();
+    }
+
+    public ArrayList <Animal> showAnimalList (String shelterId) {
+        Shelter shelter = shelterList.get(shelterId);
+        return shelter.getAnimalList ();
     }
 
 //    public static void main (String[] args) {
