5bda6d2ef8d0d5a0a0d4bcad8dff168f4ab1e960
ca4731gr <92413996+ca4731gr@users.noreply.github.com>
2022-07-19 20:05:40 -0500

    added weight unit converter and fixed shelter dataexport

--- Project2/src/main/java/project2/domain/XmlIO.java
+++ Project2/src/main/java/project2/domain/XmlIO.java
@@ -21,15 +21,15 @@ public class XmlIO implements IDataIO{
      * Read xml file in path
      * return an array list of Shelter
      */
-        private static String FILENAME = "src/main/resources/project2/sample.xml";
+        private static String filePath = MyPath.getResourcePath(fileName) + ".xml";;
 
     public static void main(String[] args) {
-        ArrayList<Animal> animalList= new ArrayList<Animal>();
+        ArrayList<Animal> animalList = new ArrayList<Animal>();
         try {
 
             SAXBuilder sax = new SAXBuilder();
             // XML is a local file
-            Document doc = sax.build(new File(FILENAME));
+            Document doc = sax.build(new File(filePath));
 
             Element rootNode = doc.getRootElement();
             List<Element> list = (List<Element>) rootNode.getChildren("Animal");
@@ -39,27 +39,36 @@ public class XmlIO implements IDataIO{
                 /** the following segment gets the info about the animal from the
                  * xml file
                  */
-              String type = target.getAttributeValue("type");
-              String id = target.getAttributeValue("id");
-              String Weight = target.getChildText("Weight");
-              String name = target.getChildText("name");
-              String ReceiptDate = target.getChildText("ReceiptDate");
-              float flWeight = Float.parseFloat(Weight); //parses the weight string into a float
+                String type = target.getAttributeValue("type");
+                String id = target.getAttributeValue("id");
+                String weightUnit = target.getAttributeValue("unit");
+                String Weight = target.getChildText("Weight");
+                String name = target.getChildText("name");
+                String ReceiptDate = target.getChildText("ReceiptDate");
+
+                float flWeight = Float.parseFloat(Weight); //parses the weight string into a float
                 Long lReceiptDate = Long.parseLong(ReceiptDate);// get the date in correct format
                 SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMddHHmmss");
-                Date dateReceipt  = dateFormat.parse(lReceiptDate.toString());
-
+                Date dateReceipt = dateFormat.parse(lReceiptDate.toString());
+                if (weightUnit == "kg") {
+                    flWeight = (float) (flWeight * 2.2);
+                }
+                else {
+                    flWeight = flWeight;
+                }
                 //adds info pulled from xml into the animal object
-              thisAnimal.setName(name);
-              thisAnimal.setAnimalId(id);
-              thisAnimal.setWeight(flWeight);
-              thisAnimal.setAnimalType(type);
-              thisAnimal.setReceiptDate(dateReceipt);
-              animalList.add(thisAnimal);//adds the newly pulled animal to the animal arraylist
+                thisAnimal.setName(name);
+                thisAnimal.setAnimalId(id);
+                thisAnimal.setWeight(flWeight);
+                thisAnimal.setAnimalType(type);
+                thisAnimal.setReceiptDate(dateReceipt);
+                animalList.add(thisAnimal);//adds the newly pulled animal to the animal arraylist
 
             }
-            }  catch (IOException | JDOMException e) {
-            e.printStackTrace(); }
+        } catch (IOException | JDOMException e) {
+            e.printStackTrace();
+        }
+    }
         /**
     @Override
     public ArrayList<Shelter> convert(String path) {
@@ -70,18 +79,18 @@ public class XmlIO implements IDataIO{
     /**
      * Export Shelter information to a xml file, placed in resources
  */
-    @Override
-    public void dataExport(Shelter shelter) throws IOException {
+        public void dataExport(Shelter shelter) throws IOException {
         ArrayList<Animal> shelterResidents = shelter.getAnimalList();
         Document doc = new Document();
         doc.setRootElement(new Element("Shelter"));
         for (int i = 0; i < shelterResidents.size(); i++) {
+            Animal currentAnim = shelterResidents.get(i);
             Element Animal = new Element("Animal");
-            Animal.setAttribute("type",this.getAnimalType());
-            Animal.setAttribute("id",this.getAnimalId());
-            Animal.addContent(new Element("Weight").setText(this.getWeight()));
-            Animal.addContent(new Element("name").setText(this.getName()));
-            Animal.addContent(new Element("ReceiptDate").setText(this.getReceiptDate()));
+            Animal.setAttribute("type",currentAnim.getAnimalType());
+            Animal.setAttribute("id",currentAnim.getAnimalId());
+            Animal.addContent(new Element("Weight").setText(currentAnim.getWeight()));
+            Animal.addContent(new Element("name").setText(currentAnim.getName()));
+            Animal.addContent(new Element("ReceiptDate").setText(currentAnim.getReceiptDate()));
         }
             XMLOutputter xmlOutputter = new XMLOutputter();
             try(FileOutputStream fileOutputStream =
@@ -90,4 +99,4 @@ public class XmlIO implements IDataIO{
             }
         }
     }
-}
+
