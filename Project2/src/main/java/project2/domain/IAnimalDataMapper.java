42c04e0851e0c48825e45e04e90bbf601afa3c61
Julia Ha <juliaha0902@gmail.com>
2022-08-04 15:56:20 -0500

    Delete old files

--- Project2/src/main/java/project2/domain/IAnimalDataMapper.java
+++
@@ -1,8 +0,0 @@
-package project2.domain;
-
-public interface IAnimalDataMapper {
-    Animal get (String animalId);
-    void update (Animal animal);
-    void insert (Animal animal);
-    void delete (String animalId);
-}
--- Project2/src/main/java/project2/domain/MyBuffer.java
+++
@@ -1,9 +0,0 @@
-package project2.domain;
-
-/**
- * A class to save user text entry
- */
-public final class MyBuffer {
-
-    public static String text = "";
-}
