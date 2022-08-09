17b4d8472e61fc56d1bda05f803f2921b29cee19
NisaM342 <97812538+NisaM342@users.noreply.github.com>
2022-08-08 23:00:46 -0500

    Update ShelterActivity.java

--- ShelterTracker/app/src/main/java/edu/metrostate/sheltertracker/controller/ShelterActivity.java
+++ ShelterTracker/app/src/main/java/edu/metrostate/sheltertracker/controller/ShelterActivity.java
@@ -4,10 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import edu.metrostate.sheltertracker.R;
+import edu.metrostate.sheltertracker.domains.Shelter;
+import edu.metrostate.sheltertracker.domains.ShelterAdapter;
+import edu.metrostate.sheltertracker.domains.ShelterTrackerApplication;
 
 public class ShelterActivity extends AppCompatActivity {
 
@@ -15,12 +21,26 @@ public class ShelterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shelter);
+
+        ListView lv = findViewById(R.id.shelter_list);
+
+//        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//                Log.i("itemClick", String.valueOf(i));
+//                Shelter shelter = ((ShelterTrackerApplication) getApplication()).getAnimalList().get(i);
+//                Intent intent = new Intent(AnimalListActivity.this, AnimalActivity.class);
+//                intent.putExtra("animalId", tvAnimalId.getanimalId());
+//                startActivity(intent);
+//            });
         Bundle extras = getIntent().getExtras();
         String shelterId;
         TextView tvAnimalId = findViewById(R.id.tvShelterId);
 
         if (extras != null) {
             shelterId = extras.getString("shelterId");
+            lv.setAdapter(new AnimalAdapter(ShelterActivity.this,
+                        ((ShelterTrackerApplication)getApplication()).showAnimalsByShelter(shelterId)));
             tvAnimalId.setText("Shelter " + shelterId);
         } else {
             tvAnimalId.setText("Invalid Shelter");
