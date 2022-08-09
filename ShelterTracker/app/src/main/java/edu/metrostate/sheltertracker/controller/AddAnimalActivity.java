bb3826b82cb99b53f31cbca608920bb921daa0c9
iimanA <91152583+iimanA@users.noreply.github.com>
2022-08-09 15:59:18 -0500

    update AddAnimalActivity

--- ShelterTracker/app/src/main/java/edu/metrostate/sheltertracker/controller/AddAnimalActivity.java
+++ ShelterTracker/app/src/main/java/edu/metrostate/sheltertracker/controller/AddAnimalActivity.java
@@ -15,13 +15,38 @@ import edu.metrostate.sheltertracker.domains.ShelterTrackerApplication;
 
 public class AddAnimalActivity extends AppCompatActivity {
 
+       Button btAddAnimal;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_animal);
-    }
+
+
+
 
     public void importAnimal (View view) {
+        Dialog dialog;
+        if(shelter.getInTaking() == true){
+            Animal Animal = new Animal(animal_name, animal_id, weight, animal_type, receipt_date,release_date);
+            shelter.addAnimal(Animal);
 
-    }
+            if(shelter.getAnimalList().contains(Animal)){
+                dialog = new AlertDialog.Builder(this).setTitle("adding animal").setCancelable(false)
+                        .setMessage(newAnimal.getAnimalId() + " Animal was added to " + shelter.getShelterName())
+                        .setPositiveButton("OK",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        dialog.dismiss();
+                                    }
+
+                                }
+            }
+        }
+        }
+            public void backHome (View view) {
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+
+        }
 }
